95072b571b473183dcb20d3b49e5cae9b7241352
dvrbancic <dubyoriginal@gmail.com>
2017-12-27 15:50:30 +0100

    added energy calculation CUSTOM Integral

--- src/PhysicsCalc.js
+++ src/PhysicsCalc.js
@@ -8,6 +8,21 @@ class PhysicsCalc {
 
   }
 
+  calculateEnergy(powArr){
+    // powArr -> {ts:mSensorData[i].timestamp, pow: sValPOW_CKP}
+    var sumArea = 0;
+    for (var i = 0; i < powArr.length-1; i += 1) {
+      var t1 = powArr[i].ts;
+      var t2 = powArr[i+1].ts;
+      var y1 = powArr[i].pow;
+      var y2 = powArr[i+1].pow;
+      dArea = (t2-t1)*(y1+y2)/2;
+      console.log("dArea[" + i + "] = " + dArea);
+      sumArea += dArea;
+    }
+    return sumArea;
+  }
+
 }
 
 
